afdaa129b11b728326f436cbfbe65d13cfafdeb2
vaibhav <vaibhavharbinger@gmail.com>
2021-08-22 22:09:14 +0530

    added a newer message in app.js - tanla industries

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to road of test drive'));
+app.get('/', (req, res) => res.send('Tanla industries'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
